25d57dbf601445f6a55ecf8f5feeebcf54f3074f
vitaliy veret <veter.luber@gmail.com>
2023-03-27 21:27:26 +0300

    update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const firstQuestion = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`${'Hello,'} ${userName}${'!'}`);
+  console.log(`${'Hello,'} ${userName}!`);
 };
 
 export default firstQuestion;
