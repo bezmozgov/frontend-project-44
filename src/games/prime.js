48bc1dde01a52084f036494da429f86ae414384c
vitaliy veret <veter.luber@gmail.com>
2023-03-27 21:31:03 +0300

    update prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,4 +1,5 @@
-import { startGame, getRandomNumber } from '../index.js';
+import { startGame } from '../index.js';
+import { getRandomNumber } from '../utils.js';
 
 const rulesGame = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
